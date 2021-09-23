a6463b6f49d045d171b852b70a1220adcc70e0c9
Klock <soup930@naver.com>
2021-09-23 21:28:02 +0900

    0923

--- jpastart/src/main/java/jpastart/main/UserMain.java
+++ jpastart/src/main/java/jpastart/main/UserMain.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 public class UserMain {
@@ -15,6 +16,8 @@ public class UserMain {
     private static JoinService joinService = new JoinService();
     private static GetUserService getUserService = new GetUserService();
     private static ChangeNameService changeNameService = new ChangeNameService();
+    private static GetUserListService listService = new GetUserListService();
+    private static WithdrawService withdrawService = new WithdrawService();
 
     public static void main(String[] args) throws IOException {
         EMF.init();
@@ -34,9 +37,11 @@ public class UserMain {
             } else if (commands[0].equalsIgnoreCase("view")) {
                 handleViewCommands(commands);
             } else if (commands[0].equalsIgnoreCase("list")) {
+                handleListCommand();
             } else if (commands[0].equalsIgnoreCase("changename")) {
                 handleChangeName(commands);
             } else if (commands[0].equalsIgnoreCase("withdraw")) {
+                handleWithdrawCommand(commands);
             } else {
                 System.out.println("올바른 명령어를 입력하세요.");
             }
@@ -93,4 +98,33 @@ public class UserMain {
             System.out.println("존재하지 않습니다.");
         }
     }
+
+
+    private static void handleListCommand() {
+        List<User> users = listService.getAllUsers();
+        if (users.isEmpty()) {
+            System.out.println("사용자가 없습니다.");
+        } else {
+            users.forEach(user ->
+                    System.out.printf("| %s | %s | %tY-%<tm-%<td |\n",
+                            user.getEmail(), user.getName(), user.getCreateDate()));
+        }
+    }
+
+
+    private static void handleWithdrawCommand(String[] cmd) {
+        if (cmd.length != 2) {
+            System.out.println("명령어가 올바르지 않습니다.");
+            System.out.println("사용법 : withdraw 이메일");
+            return;
+        }
+        try {
+            withdrawService.withdraw(cmd[1]);
+            System.out.println("탈퇴처리 했습니다.");
+        } catch (UserNotFoundException e) {
+            System.out.println("존재하지 않습니다.");
+        }
+
+    }
+
 }
